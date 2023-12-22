de6729a8d71cf87c18f721ceeb2acfdeab29a4fe
Jen Ziel <jenziel400@gmail.com>
2023-12-22 16:22:31 -0500

    Add a fetch call to the deployed newsapi.org  search endpoint

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -8,3 +8,13 @@ export function getTopHeadlines(){
       }
    )
 }
+export function getSearchResults(query){
+    return fetch(`https://newsapi.org/v2/everything?q=${query}&from=2023-11-22&sortBy=publishedAt&apiKey=2d9966d5b2fd4408bc544f2b0a6c06ae`).then(
+     (response) => {
+         if (!response.ok) {
+           throw new Error(`Movies not found.`);
+         }
+         return response.json();
+       }
+    )
+ }
